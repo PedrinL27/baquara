9ed65d97f711473ea872977bfca191e6eb704cd9
Pedro Neto <pedrolangelineto0@gmail.com>
2026-03-05 15:05:35 -0300

    Estrutura inicial do projeto Django

---
+++ application/core/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from core.views import home
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", home, name="home"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
